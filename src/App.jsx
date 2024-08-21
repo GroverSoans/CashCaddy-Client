cb2a4a26a53be28612dde2194207079e999bb8b8
GroverSoans <groversoans@Grovers-Air.zyxel.com>
2024-08-21 15:54:34 -0500

    Plaid Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,61 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { usePlaidLink } from 'react-plaid-link';
 
 axios.defaults.baseURL = "http://localhost:8000"
 
+function PlaidAuth({publicToken}) {
+  const [account, setAccount] = useState();
+
+
+  useEffect(() => {
+    async function fetchData() {
+      let accessToken = await axios.post("/exchange_public_token", {public_token: publicToken})
+      console.log("accessToken", accessToken.data);
+      const auth = await axios.post("/auth", {access_token: accessToken.data.accessToken})
+      console.log("auth data", auth.data)
+      setAccount(auth.data.numbers.ach[0])
+    }
+    fetchData()
+  }, [])
+  return account && (
+    <div>
+          <p>Account number: {account.account}</p>
+          <p>Routing number: {account.routing}</p>
+    </div>
+
+  );
+}
+
 function App() {
+  const [linkToken, setLinkToken] = useState();
+  const [publicToken, setPublicToken] = useState();
+
   useEffect(()=>{
     async function fetch() {
-      const response = await axios.post("/hello", {name: "grover"});
+      const response = await axios.post("/create_link_token");
       console.log("response ", response.data);
+      setLinkToken(response.data.link_token)
     }
     fetch()
   }, [])
 
-  return (
-    <div>Hello World</div>
-    
-  )
+
+  const { open, ready } = usePlaidLink({
+    token: linkToken,
+    onSuccess: (public_token, metadata) => {
+    // send public_token to server
+    console.log('success', public_token, metadata)
+    setPublicToken(public_token)
+    },
+  });
+
+
+  return publicToken ? (<PlaidAuth publicToken={publicToken}/>) : (
+    <button onClick={() => open()} disabled={!ready}>
+      Connect a bank account
+    </button>
+  );
 }
 
 export default App
